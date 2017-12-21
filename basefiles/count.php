6edcf52c50479464ca0f896091283085dfb1af51
Elanis <elanis@hotmail.com>
2017-12-21 20:08:51 +0100

    Final count.php code

--- basefiles/count.php
+++ basefiles/count.php
@@ -7,23 +7,5 @@ $cmd = new MongoDB\Driver\Command(["count" => "hashlists", "query" => []]);
 
 // Result
 $result = $manager->executeCommand('***REMOVED***', $cmd);
-$results = $result->toArray();
 
-// Get Total Online In 1 Hour Ago
-var_dump($result);
-echo "
------------------------------
-";
-var_dump($results);
-echo "
------------------------------
-";
-var_dump($results[0]);
-echo "
------------------------------
-";
-var_dump(json_decode(json_encode($results[0]), true)['n']);
-echo "
------------------------------
-";
-echo json_decode(json_encode($results[0]), true)['n'];
+echo json_decode(json_encode($result->toArray()[0]), true)['n'];

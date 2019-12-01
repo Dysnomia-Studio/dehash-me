41d8c3e42acccf8efe0df0ac4fb44015c89babbf
Elanis <elanis@hotmail.com>
2019-12-01 12:14:53 +0100

    Fix some issues reported by sonarqube

--- Dysnomia.DehashMe.WebApp/wwwroot/js/script.js
+++ Dysnomia.DehashMe.WebApp/wwwroot/js/script.js
@@ -1,49 +1,47 @@
 ﻿// Hash count
 async function hashCount() {
-    const countHashResponse = await fetch('/count');
+	const countHashResponse = await fetch('/count');
 
-    console.log(countHashResponse);
+	if (countHashResponse.status !== 200) { return; }
+	let countHash = await countHashResponse.text();
 
-    if (countHashResponse.status !== 200) { return; }
-    let countHash = await countHashResponse.text();
+	const text = [];
+	let i = countHash.length;
+	while (i > 2) {
+		i -= 3;
+		text.unshift(countHash.slice(i));
+		countHash = countHash.slice(0, i);
+	}
+	text.unshift(countHash);
 
-    const text = [];
-    let i = countHash.length;
-    while (i > 2) {
-        i -= 3;
-        text.unshift(countHash.slice(i));
-        countHash = countHash.slice(0, i);
-    }
-    text.unshift(countHash);
-
-    $('#hashCount').text(text.filter((d) => d != ''));
+	$('#hashCount').text(text.filter((d) => d !== ''));
 }
 
 // SVG Background
-var count = 0;
+let count = 0;
 $(document).ready(() => {
-    $('#background svg').attr("width", $(window).innerWidth());
-    $('#background svg').attr("height", $(window).innerHeight());
+	$('#background svg').attr('width', $(window).innerWidth());
+	$('#background svg').attr('height', $(window).innerHeight());
 
-    setInterval(function () {
-        let i = count++;
-        count %= 10;
-        $('#background-' + i).append('<circle id="bubble-' + i + '" fill="#27ae60" fill-opacity="0.1" class="background-bubble" cx="' +
-            Math.floor(Math.random() * $(window).innerWidth()) + '" cy="' +
-            Math.floor(Math.random() * $(window).innerHeight()) + '" r="0" />');
+	setInterval(function () {
+		const i = count++;
+		count %= 10;
+		$('#background-' + i).append('<circle id="bubble-' + i + '" fill="#27ae60" fill-opacity="0.1" class="background-bubble" cx="' +
+			Math.floor(Math.random() * $(window).innerWidth()) + '" cy="' +
+			Math.floor(Math.random() * $(window).innerHeight()) + '" r="0" />');
 
-        $('#background-' + i).html($('#background-' + i).html());
+		$('#background-' + i).html($('#background-' + i).html());
 
-        setTimeout(function () {
-            $('#bubble-' + i).remove();
-            $('#background-' + i).html($('#background-' + i).html());
-        }, 3000);
-    }, 300);
+		setTimeout(function () {
+			$('#bubble-' + i).remove();
+			$('#background-' + i).html($('#background-' + i).html());
+		}, 3000);
+	}, 300);
 
-    $(window).resize(function () {
-        $('#background svg').attr("width", $(window).innerWidth());
-        $('#background svg').attr("height", $(window).innerHeight());
-    });
+	$(window).resize(function () {
+		$('#background svg').attr('width', $(window).innerWidth());
+		$('#background svg').attr('height', $(window).innerHeight());
+	});
 
-    hashCount();
+	hashCount();
 });

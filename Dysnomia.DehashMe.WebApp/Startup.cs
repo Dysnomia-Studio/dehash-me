575cf224e2670426d9ce383f8359d226903e2e4e
Elanis <elanis@hotmail.com>
2019-12-08 20:56:09 +0100

    Automated Tests on CI envionement

--- Dysnomia.DehashMe.WebApp/Startup.cs
+++ Dysnomia.DehashMe.WebApp/Startup.cs
@@ -32,7 +32,7 @@ namespace Dysnomia.DehashMe.WebApp {
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
+			if (env.IsDevelopment() || env.IsEnvironment("Testing")) {
 				app.UseDeveloperExceptionPage();
 			} else {
 				app.UseExceptionHandler("/Home/Error");

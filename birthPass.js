4af49c0826e0da7bf79c5e4e3e8aa82f337c5ef3
Elanis <elanis@hotmail.com>
2020-05-13 16:57:44 +0200

    Birthpass as default script

--- birthPass.js
+++ birthPass.js
@@ -2,21 +2,21 @@ require('./passCalc')();
 
 let j = 1,
 	m = 1,
-	y = 1900,
+	y = 0,
 	total = false;
 
 function birthPass() {
-	let text = "";
+	let text = '';
 
-	if(j < 10)	{ text += "0" + j; }
+	if(j < 10)	{ text += '0' + j; }
 	else 		{ text += j; }
 
-	if(m < 10)	{ text += "0" + m; }
+	if(m < 10)	{ text += '0' + m; }
 	else 		{ text += m; }
 
-	if(total)	{ text += y; } 
-	else		{ 
-		if(y%100 < 10)	 { text += "0" + y%100; }
+	if(total)	{ text += y; }
+	else		{
+		if(y%100 < 10)	 { text += '0' + y%100; }
 		else		 { text += y%100; }
 	}
 
@@ -28,6 +28,10 @@ function birthPass() {
 
 		if(j>31) { j = 1; m++; }
 		if(m>31) { m = 1; y++; console.log(y); }
+
+		if(y > 3000) {
+			return;
+		}
 
 		birthPass();
 	});

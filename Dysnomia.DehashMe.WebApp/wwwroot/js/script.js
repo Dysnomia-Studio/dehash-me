c263d69e867f714fc450e2334c0b3f5b036e5d1a
Elanis <elanis@hotmail.com>
2021-03-10 11:08:08 +0100

    #1108 - Remove jQuery

--- Dysnomia.DehashMe.WebApp/wwwroot/js/script.js
+++ Dysnomia.DehashMe.WebApp/wwwroot/js/script.js
@@ -16,34 +16,42 @@ async function hashCount() {
 	}
 	text.unshift(countHash);
 
-	$('#hashCount').text(text.filter((d) => d !== ''));
+	document.getElementById('hashCount').innerText = text.filter((d) => d !== '');
 }
 
 // SVG Background
 let count = 0;
-$(document).ready(() => {
-	$('#background svg').attr('width', $(window).innerWidth());
-	$('#background svg').attr('height', $(window).innerHeight());
 
+function ready(fn) {
+	if (document.readyState != 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+ready(() => {
 	setInterval(function () {
 		const i = count;
 		count++;
 		count %= 10;
-		$('#background-' + i).append('<circle id="bubble-' + i + '" fill="#27ae60" fill-opacity="0.1" class="background-bubble" cx="' +
-			Math.floor(Math.random() * $(window).innerWidth()) + '" cy="' +
-			Math.floor(Math.random() * $(window).innerHeight()) + '" r="0" />');
 
-		$('#background-' + i).html($('#background-' + i).html());
+		let bgElt = document.getElementById('background-' + i);
+		bgElt.innerHTML = bgElt.innerHTML + '<circle id="bubble-' + i + '" fill="#27ae60" fill-opacity="0.1" class="background-bubble" cx="' +
+			Math.floor(Math.random() * window.innerWidth) + '" cy="' +
+			Math.floor(Math.random() * window.innerHeight) + '" r="0" />'
 
 		setTimeout(function () {
-			$('#bubble-' + i).remove();
-			$('#background-' + i).html($('#background-' + i).html());
+			document.getElementById('bubble-' + i).remove();
+			bgElt.innerHTML = bgElt.innerHTML;
 		}, 3000);
 	}, 300);
 
-	$(window).resize(function () {
-		$('#background svg').attr('width', $(window).innerWidth());
-		$('#background svg').attr('height', $(window).innerHeight());
+	window.addEventListener('resize', function () {
+		for (const element of document.querySelectorAll('#background svg')) {
+			element.setAttribute('tabindex', 'width', window.innerWidth);
+			element.setAttribute('tabindex', 'height', window.innerHeight);
+		}
 	});
 
 	hashCount();
